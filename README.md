941bf2fa3b9c408bf954f63d29a6b86d881df445
Phillip Kovalev <kaero@yandex-team.ru>
2013-05-22 20:31:56 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,311 @@
+# Terror
+
+Base error class which will help you organize errors, generate informative logs and as result grep logs more effectively.
+
+## Trivial usage
+
+```javascript
+var Terror = require('terror'),
+
+    // declare error type with custom error codes and messages
+    MyError = Terror.create('MyError', {
+        STRANGE_THING_HAPPENS : [ 2001, 'Something strange happens' ]
+    });
+
+try {
+    // ...
+
+    if (typeof userInput === 'undefined') {
+        // throw error
+        throw MyError.createError(MyError.CODES.STRANGE_THING_HAPPENS);
+        // or
+        // throw new MyError(MyError.CODES.STRANGE_THING_HAPPENS);
+    }
+} catch (err) {
+    // ensureError method returns err if it is an instance of Terror,
+    // otherwise wrap Error instance in the Terror with default code UNKNOWN_ERROR
+    MyError.ensureError(err)
+        .log();
+}
+```
+
+## Constructor 
+
+```javascript
+new Terror(code, message);
+new Terror(code);
+new Terror();
+new Terror(null, message);
+```
+
+Also can be called as the function, then calls self as the constructor internally and returns created instance.
+
+Both argumetns are not required:
+ * if only `code` passed, message will got from `Terror.MESSAGES` hash by the `code` value as the key;
+ * if `code` is absent, will be used default value `Terror.CODES.UNKNOWN_ERROR`.
+
+## Constructor methods
+
+### create(name, constructor, codes)
+
+Returns constructor inherited from Terror or it's inheritor. You must specify `name` for logging purpose.
+
+Arguments:
+ * `{String} name` – error name for logging;
+ * `{Function} constructor` – optional, don't forget to call `<YouErrorClass>.__super.apply(this, arguments)`.
+ * `{Object} codes` - optional, CODES hash { CODE_NAME: [ 2001, 'error message' ], … }
+
+Example:
+```javascript
+var AppError = Terror.create('AppError'),
+    ControllerError = AppError.create('ControllerError', function(controller) {
+        ControllerError.__super.apply(this, Array.prototype.slice.call(arguments, 1));
+        this.controller = controller;
+    }, {
+        IO_ERROR : [2001, 'Broken IO'],
+        FS_ERROR : [2002, 'Broken %fsName% file-system']
+    });
+```
+
+### extendCodes(codes)
+
+Extends `CODES` and associated `MESSAGES` hashes using `codes` declaration. Method throws an error if codes uniqueness violated by extention.
+
+Example:
+```javascript
+var AppError = Terror.create('AppError')
+				.extendCodes({
+						BROKEN_CONFIG : [2001, 'Looks like configuration file is broken.'],
+						DB_CONNECTION_FAILED : [2002, 'Can not connect to database %db_host%']
+				});
+
+// now you can use AppError.CODES.BROKEN_CONFIG & AppError.CODES.DB_CONNECTION_FAILED to produce errors
+AppError.createError(AppError.CODES.BROKEN_CONFIG);
+```
+
+Previous example produces following `CODES` and `MESSAGES` structures:
+```javascript
+AppError.CODES = {
+		UNKNOWN_ERROR : 101,
+        BROKEN_CONFIG : 2001,
+		DB_CONNECTION_FAILED : 2002
+}
+
+AppError.MESSAGES = {
+    101 : 'Unknow error',
+    2001 : 'Looks like configuration file is broken.',
+    2002 : 'Can not connect to database %db_host%'
+}
+```
+
+Where error `101` inherited from the `Terror`.
+
+### setLogger(logger)
+
+Set function which error class and its iheritors will use for logging.
+It called by `log` method with two argumetns: `message` and `level`.
+
+Example:
+```javascript
+var log = [],
+    logger = function(message, level) {
+        log.push([new Date(), level, message].join());
+    },
+    MyError = Terror.create('MyError').setLogger(logger);
+
+MyError.createError().log();
+
+console.log(log.join('\n'));
+```
+
+### createError(code, message | originalError | data)
+
+Creates new `Terror` or its inheritor instance.
+
+Arguments:
+* `{Number} [code]` – error code from `CODES` hash, `Terror.CODES.UNKNOWN_ERROR` used as default value;
+* `{String|Error|Terror|Object} [message|originalError|data]`
+** if 2nd argument is String then use it as original error message;
+** if 2nd argument is Error then use its message and call-stack to format original message;
+** if 2nd argument is an Object then replace error message placeholders with provided data.
+
+Example:
+```javascript
+var MyError = Terror.create('MyError', {
+    IO_ERROR : [2001, 'Broken IO'],
+    FS_ERROR : [2002, 'Broken %fsName% file-system']
+});
+
+// valid createError calls
+
+// "101 Terror: Unknown error"
+Terror.createError();
+
+// same as above
+Terror.createError(Terror.CODES.UNKNOWN_ERROR);
+
+// "101 MyError: Unknown error"
+MyError.createError();
+
+// "2001 MyError: Broken IO"
+MyError.createError(MyError.CODES.IO_ERROR);
+
+// "2001 MyError: Broken IO (Error: kbd int broken)"
+MyError.createError(MyError.CODES.IO_ERROR, new Error('kbd int broken'));
+
+// "2002 MyError: Broken vfat file-system"
+MyError.createError(MyError.CODES.FS_ERROR, {fsName : 'vfat'});
+
+// fails, because Terror.MESSAGES[2001] is undefined
+Terror.createError(MyError.CODES.IO_ERROR);
+```
+
+### ensureError(originalError, code)
+
+Returns `originalError` if it's an instance of `Terror`.
+Else wrap `originalError` into new `Terror` instance using `code` or `UNKNOWN_ERROR` code if second argument is absent.
+
+Example:
+```javascript
+var MyError = Terror.create('MyError', {
+        EMPTY_MESSAGE : [1000, 'Message string is empty'],
+        UNEXPECTED_ERROR : [1999, 'Unexpected error']
+    }),
+    arr = [ { msg : "hello" }, { msg : "" }, {} ];
+
+arr.forEach(function(item) {
+    try {
+        if (item.msg.length > 0) {
+            console.log(item.msg);
+        } else {
+            throw MyError.createError(MyError.CODES.EMPTY_MESSAGE);
+        }
+    } catch (err) {
+        throw MyError.ensureError(err, MyError.CODES.UNEXPECTED_ERROR)
+            .log();
+    }
+})
+```
+
+## Constructor fields
+
+### CODES and MESSAGES
+
+Hashes contains error names to codes and codes to messages mappings. Proposed to generate them by `extendCodes` method call, but if you want to override inherited code, you can do it manually using those hashes.
+
+Example:
+```javascript
+var MyError = Terror.create('MyError');
+
+// following call leads to error, because UNKNOWN_ERROR name already exist – it's inherited from Terror
+MyError.extendCodes({
+    UNKNOWN_ERROR : [2001, 'Unbelievable!']
+});
+
+// it's fine
+MyError.CODES.UNKNOWN_ERROR = 2001;
+MyError.MESSAGES['2001'] = 'Unbelievable!';
+```
+
+Also you can simply reset inherited codes:
+```javascript
+// @see http://npm.im/extend
+MyError.CODES = extend({}, Terror.CODES);
+MyError.MESSAGES = extend({}, Terror.MESSAGES);
+```
+
+### DEFAULT_LOG_LEVEL
+
+Log level, which `log` method use as default value if the `level` argument isn't passed.
+
+It equals `"ERROR"` by default.
+
+## Prototype methods
+
+### log(level)
+
+Log error with specified `level`. If method called twice or more for the same instance, logger will be called by first `log` call only.
+
+Arguments:
+* `{*} level` – optional argument, any type accepted by logger; default value equals `constructor.DEFAULT_LOG_LEVEL`.
+
+Example:
+```javascript
+var Terror = require('terror'),
+		terr;
+
+Terror.DEFAULT_LOG_LEVEL = 'INFO';
+
+try {
+		console.lag("hello!");
+} catch (err) {
+		terr = Terror.ensureError(err);
+		terr.log()
+				.log('warn')
+				.log('error');
+}
+```
+
+`terr` will be logged once with 'INFO' error level.
+
+### bind(data)
+
+ Fill an error message with values from `data` object properties.
+
+Arguments:
+* `{Object} data` – hash, where key is a placeholder name to be replaced, value – a data to replace with.
+
+Example:
+```javascript
+var IOError = Terror.create('MyError', {
+        IO_ERROR : [2001, 'Broken IO pipe "%pipe%"']
+    });
+
+IOError.createError(IOError.CODES.IO_ERROR)
+		.bind({ pipe : 'main bus' })
+		.log('PANIC');
+// or
+IOError.createError(IOError.CODES.IO_ERROR, { pipe : 'main bus' })
+		.log('PANIC');
+```
+
+## Internal, but usefull prototype methods
+
+Following methods used by built-in logger routine, but may be usefull for any simple text logger.
+
+### logMultilineError(message, level, logger)
+
+Formats error message and calls `logger(formattedMessage)`.
+
+Formatting is optimized for output call-stacks to the text files and future grep through.
+
+Format:
+```
+YYYY-MM-DD HH:MM:SS LEVEL CODE CLASS: MESSAGE
+YYYY-MM-DD HH:MM:SS >>>>>   CALL STACK ROW 1
+…
+YYYY-MM-DD HH:MM:SS >>>>>   CALL STACK ROW N
+```
+
+Example:
+```
+2013-04-15 12:50:57 ERROR 100 Terror: Unknown error (TypeError: Object #<Console> has no method 'lag')
+2013-04-15 12:50:57 >>>>> TypeError: Object #<Console> has no method 'lag'
+2013-04-15 12:50:57 >>>>>     at repl:2:9
+2013-04-15 12:50:57 >>>>>     at REPLServer.self.eval (repl.js:110:21)
+2013-04-15 12:50:57 >>>>>     at repl.js:249:20
+2013-04-15 12:50:57 >>>>>     at REPLServer.self.eval (repl.js:122:7)
+2013-04-15 12:50:57 >>>>>     at Interface.<anonymous> (repl.js:239:12)
+2013-04-15 12:50:57 >>>>>     at Interface.EventEmitter.emit (events.js:95:17)
+2013-04-15 12:50:57 >>>>>     at Interface._onLine (readline.js:202:10)
+2013-04-15 12:50:57 >>>>>     at Interface._line (readline.js:531:8)
+2013-04-15 12:50:57 >>>>>     at Interface._ttyWrite (readline.js:754:14)
+2013-04-15 12:50:57 >>>>>     at ReadStream.onkeypress (readline.js:99:10)
+```
+
+### formatErrorTimestamp(date)
+
+Method is internally used by the `logMultilineError` to create log line timestamp formatted as `YYYY-MM-DD HH:MM:SS`.
+
+Arguments:
+* `{Date} date` – oh, assumed it's really Date instance always, no casting, no magic.
---
+++ test/terror.js
@@ -0,0 +1,264 @@
+var Terror = require('../lib/terror'),
+    originalConsoleLog = console.log,
+    log = [];
+
+function restoreConsoleLog() {
+    console.log = originalConsoleLog;
+}
+
+function catchConsoleLog() {
+    log = [];
+
+    console.log = function() {
+        log.push(Array.prototype.join.apply(arguments));
+    };
+}
+
+module.exports = {
+    constructor : function(test) {
+        var testErrorName = 'TestError',
+            errorMessage = 'Test Error message',
+            TestError = Terror.create(testErrorName),
+            testError = new TestError(TestError.CODES.UNKNOWN_ERROR),
+            testError2 = new TestError(
+                TestError.CODES.UNKNOWN_ERROR,
+                TestError.MESSAGES[TestError.CODES.UNKNOWN_ERROR]),
+            terrorDefaultCode = new TestError(),
+            terrorViaCtor = new TestError(TestError.CODES.UNKNOWN_ERROR, errorMessage);
+
+        test.ok(testError instanceof TestError, 'inheritance check #1');
+        test.ok(testError instanceof Error, 'inheritance check #2');
+        test.ok(testError instanceof Object, 'inheritance check #3');
+
+        test.strictEqual(testError.message, testError2.message, 'constructor call with `code` argument only');
+        test.strictEqual(testError.message, Terror.MESSAGES[Terror.CODES.UNKNOWN_ERROR], 'message by code selection');
+        test.strictEqual(TestError.prototype.name, testErrorName, '`name` prototype property set');
+        test.ok(Object.prototype.hasOwnProperty.call(TestError.prototype,'name'), 'Terror inheritor has own prototype property `name`');
+        test.strictEqual(testError.name, testErrorName, '`name` property available in the instance');
+
+        test.strictEqual(terrorDefaultCode.code, TestError.CODES.UNKNOWN_ERROR, 'use default code, if no one passed to createError');
+        test.strictEqual(terrorViaCtor.message, errorMessage, 'message passed to constructor as string');
+
+        test.done();
+    },
+
+    "createError & extendCodes" : function(test) {
+        var testCodes = { USER_ERROR : [201, 'User "%username%" leads to error'],
+                ABSOLUTE_ERROR : [0, 'Our World Is Broken...']
+            },
+            TestError = Terror.create('TestError').extendCodes(testCodes),
+            TestErrorWithCodes = Terror.create('TestErrorWithCodes', testCodes),
+            errorMessage = 'Test Error message',
+            userName = 'john_doe',
+            originalError = new Error(errorMessage),
+            terrorByError = TestError.createError(null, originalError),
+            terrorWithData = TestError.createError(TestError.CODES.USER_ERROR, { username : userName }),
+            terrorWithMessage = TestError.createError(TestError.CODES.USER_ERROR, errorMessage),
+            terrorWithZeroErrorCode = TestError.createError(TestError.CODES.ABSOLUTE_ERROR);
+
+        test.strictEqual(terrorByError.code, TestError.CODES.UNKNOWN_ERROR, 'use default code, if no one passed to createError');
+        test.strictEqual(terrorByError.originalError, originalError, 'Error instance passed to createError');
+
+        test.strictEqual(terrorWithZeroErrorCode.code, TestError.CODES.ABSOLUTE_ERROR, 'error with zero code doesn\'t use default code');
+
+        test.strictEqual(terrorWithMessage.code, TestError.CODES.USER_ERROR, 'error code passed to createError with custom message');
+        test.strictEqual(terrorWithMessage.originalError, errorMessage, 'custom message passed to createError');
+
+        test.notStrictEqual(TestError.CODES, TestError.__super.CODES, 'static field CODE deep copied');
+        test.notStrictEqual(TestError.MESSAGES, TestError.__super.MESSAGES, 'static field MESSAGES deep copied');
+
+        Object.getOwnPropertyNames(TestError.__super.CODES).forEach(function(codeName) {
+            var code = TestError.CODES[codeName];
+
+            test.strictEqual(
+                TestError.CODES[codeName],
+                TestError.__super.CODES[codeName],
+                ['error code "', codeName, '" inheritance from ', TestError.__super.prototype.name].join(''));
+            test.strictEqual(
+                TestError.MESSAGES[code],
+                TestError.__super.MESSAGES[code],
+                ['error message "', codeName, '" : ', code, ' inheritance from ', TestError.__super.prototype.name].join(''));
+        });
+
+        Object.getOwnPropertyNames(testCodes).forEach(function(codeName) {
+            var code = TestError.CODES[codeName];
+
+            [TestError, TestErrorWithCodes].forEach(function(toTest) {
+                test.strictEqual(
+                    toTest.CODES[codeName],
+                    testCodes[codeName][0],
+                    ['Terror inheritor "',toTest.prototype.name,'" has it\'s own error code "', codeName, '"'].join(''));
+                test.strictEqual(
+                    toTest.MESSAGES[code],
+                    testCodes[codeName][1],
+                    ['Terror inheritor "',toTest.prototype.name,'" has it\'s own error message "', codeName, '" : ', code].join(''));
+            });
+
+        });
+
+        test.strictEqual(
+            terrorWithData.message,
+            TestError.MESSAGES[TestError.CODES.USER_ERROR].replace('%username%', userName),
+            'message data bindings via createError');
+
+        test.done();
+    },
+
+    "logError, setLogger, logger and error formatting" : function(test) {
+        var errorClassName = 'TestError',
+            testCodes = { USER_ERROR : [201, 'User "%username%" leads to error at %time%'] },
+            TestError = Terror.create(errorClassName).extendCodes(testCodes),
+            errorMessage = 'Test Error message',
+            userName = 'john_doe',
+            time = '12:05',
+            errorLevel = 'panic',
+            originalError = new Error(errorMessage),
+            terrorByError = TestError.createError(null, originalError),
+            terrorWithData = TestError.createError(TestError.CODES.USER_ERROR, { username : userName, time : time }),
+            terrorWithMessage = TestError.createError(TestError.CODES.USER_ERROR, errorMessage),
+            logger = function(message, level) {
+                log = [];
+
+                this.logMultilineError(message, level, function(msgRow) {
+                    log.push(msgRow);
+                });
+            },
+            date,
+            dateLogged;
+
+        catchConsoleLog();
+        terrorByError.log();
+        restoreConsoleLog();
+
+        test.ok(log.length > 1, 'multiline log');
+
+        test.strictEqual(log[0].split(' ')[2], TestError.DEFAULT_ERROR_LEVEL, 'default error level');
+
+        test.strictEqual(
+            log[1].split(' ')[2],
+            TestError.DEFAULT_ERROR_LEVEL.replace(/./g, '>'),
+            'error level replaced with ">"');
+
+        test.strictEqual(log[0].split(' ')[4].replace(/:$/g, ''), errorClassName, 'log error class name');
+
+        test.strictEqual(
+            log[0].split(' ').slice(5).join(' ').replace(/^.*\(Error: (.*)\)$/g, "$1"),
+            errorMessage,
+            'log original error message');
+
+        date = new Date();
+        dateLogged = new Date(log[log.length - 1].split(' ').slice(0, 2).join(' '));
+
+        // dirty aproximated to half-minute date and time comparison
+        test.ok(date.getTime() - dateLogged.getTime() < 30000, 'log timestamp');
+
+        catchConsoleLog();
+        terrorByError.log();
+        restoreConsoleLog();
+
+        test.strictEqual(log.length, 0, 'log error only once');
+
+        catchConsoleLog();
+        terrorWithMessage.log(errorLevel);
+        restoreConsoleLog();
+
+        test.strictEqual(
+            log[0].split(' ').slice(5).join(' ').replace(/.*\((.*)\)$/g, "$1"),
+            errorMessage,
+            'append original error message');
+        test.strictEqual(
+            log[0].split(' ')[2],
+            errorLevel.toUpperCase(),
+            'custom error level passed to logError');
+        test.strictEqual(
+            log[0].split(' ')[3],
+            String(TestError.CODES.USER_ERROR),
+            'custom error code');
+
+        catchConsoleLog();
+        terrorWithData.log();
+        restoreConsoleLog();
+
+        test.strictEqual(
+            log[0].split(' ').slice(5).join(' '),
+            TestError.MESSAGES[TestError.CODES.USER_ERROR].replace('%username%', userName).replace('%time%', time),
+            'data binding via createError');
+
+        test.done();
+    },
+
+    "bind" : function(test) {
+        var testCodes = { USER_ERROR : [201, 'User "%username%" leads to error at %time%'] },
+            TestError = Terror.create('TestError').extendCodes(testCodes),
+            userName = 'john_doe',
+            time = '12:04',
+            terrorNotBinded = TestError.createError(TestError.CODES.USER_ERROR),
+            terrorBinded = TestError
+                .createError(TestError.CODES.USER_ERROR)
+                .bind({ username : userName, time : time });
+
+        catchConsoleLog();
+        terrorNotBinded.log();
+        restoreConsoleLog();
+
+        test.strictEqual(
+            log[0].split(' ').slice(5).join(' '),
+            TestError.MESSAGES[TestError.CODES.USER_ERROR],
+            'not binded error message contains placeholder');
+
+        catchConsoleLog();
+        terrorBinded.log();
+        restoreConsoleLog();
+
+        test.strictEqual(
+            log[0].split(' ').slice(5).join(' '),
+            TestError.MESSAGES[TestError.CODES.USER_ERROR].replace('%username%', userName).replace('%time%', time),
+            'bind placeholders replacement done');
+
+        test.done();
+    },
+
+    "ensureError" : function(test) {
+        var rawError = new Error('test error'),
+            terror = new Terror(null, 'test error'),
+            ensuredError,
+            code = 42,
+            zeroCode = 0,
+            ensuredErrorWithCode,
+            ensuredErrorWithZeroCode;
+
+        try {
+            throw rawError;
+        } catch (err) {
+            ensuredError = Terror.ensureError(err);
+
+            test.notEqual(err, ensuredError, 'ensured error is not the same as raw Error');
+            test.strictEqual(ensuredError.originalError, err, 'ensured error refers to the original raw Error');
+            test.ok(ensuredError instanceof Terror, 'ensured error is an instance of the Terror');
+        }
+
+        ensuredError = undefined;
+
+        try {
+            throw rawError;
+        } catch (err) {
+            ensuredErrorWithCode = Terror.ensureError(err, code);
+            ensuredErrorWithZeroCode = Terror.ensureError(err, zeroCode);
+
+            test.strictEqual(ensuredErrorWithCode.code, code, 'ensured error code is the same as passed');
+            test.strictEqual(ensuredErrorWithZeroCode.code, zeroCode, 'ensured error zero code is still zero');
+        }
+
+        ensuredError = undefined;
+
+        try {
+            throw terror;
+        } catch (err) {
+            ensuredError = Terror.ensureError(err);
+
+            test.strictEqual(ensuredError, terror, 'original Terror instance is the same object as of ensured error');
+        }
+
+        test.done();
+    }
+};
